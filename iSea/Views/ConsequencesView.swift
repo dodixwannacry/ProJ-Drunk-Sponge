6140e92805ac0e1ad9886e1185c832e6620380de
dodixwannacry <132576872+dodixwannacry@users.noreply.github.com>
2023-10-24 12:12:15 +0200

    zio pera

--- iSea/Views/ConsequencesView.swift
+++ iSea/Views/ConsequencesView.swift
@@ -9,10 +9,41 @@ import SwiftUI
 
 struct ConsequencesView: View {
     var body: some View {
-        Text("Consequences screen")
+        NavigationStack {
+            VStack {
+                Text("Consequences")
+                    .font(.largeTitle)
+                    .bold()
+                
+                    .frame(width: 250)
+                ZStack(alignment: .topLeading) {
+                    Image("consequences")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(height: 180)
+                    
+                    
+                    Rectangle().fill(LinearGradient(colors: [.clear, .black.opacity(0.3)], startPoint: .top, endPoint: .bottom))
+                        .frame(height: 185)
+                        .cornerRadius(29)
+                    VStack(alignment: .leading) {
+                        Text("The consequences on sea pollution").font(.largeTitle)
+                            .bold()
+                            .padding()
+                    }
+                    
+                    .foregroundStyle(Color.white)
+                    .padding()
+                }
+                .cornerRadius(18)
+                .padding(.top)
+                .padding(.horizontal)
+            }
+        }
     }
 }
+        #Preview {
+            ConsequencesView()
+        }
+    
 
-#Preview {
-    ConsequencesView()
-}
--- iSea/Views/ModalView.swift
+++ iSea/Views/ModalView.swift
@@ -10,8 +10,8 @@ import SwiftUI
 struct ModalView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+          }
+ }
 
 #Preview {
     ModalView()
