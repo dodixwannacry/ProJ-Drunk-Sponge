24f5b1c704e62bf1c531056ff2460dde477ff64a
ItsIla23 <ipoziello23@fed.idserve.net>
2023-10-27 12:12:38 +0200

    gggrdtf

--- iSea/Views/ActivitiesView.swift
+++ iSea/Views/ActivitiesView.swift
@@ -13,19 +13,7 @@ struct ActivitiesView: View {
     var viewModel = ActivitiesViewModel()
     
     var body: some View {
-       
             NavigationStack {
-<<<<<<< HEAD
-                    HStack {
-                        Text("Activities").font(.largeTitle).bold()
-                            Spacer()
-                            .frame(width: 180)
-                        
-                        NavigationLink(destination: UserImpactView()){
-                                Image(systemName: "chart.bar.fill")
-                                    .font(.subheadline)
-                                    .foregroundColor(.black)
-=======
                 HStack {
                     Text("Activities").font(.largeTitle).bold()
                         Spacer()
@@ -57,40 +45,14 @@ struct ActivitiesView: View {
                                         .foregroundStyle(Color.white)
                                         .padding()
                                 }
-
+                                
                             }
                                 .cornerRadius(15)
                                 .padding(.horizontal)
->>>>>>> c7446b133c64ede156ee0491d49b4256cb6b1964
                         }
                     }
+                }
                     
-                    ScrollView() {
-                        VStack() {
-                            ForEach(viewModel.activity) { activity in
-                                NavigationLink(destination: ActivityChartView().navigationBarTitle(activity.name)) {
-                                    ZStack(alignment: .bottomLeading) {
-                                        Image(activity.imageName)
-                                            .resizable()
-                                            .aspectRatio(contentMode: .fill)
-                                            .frame(height: 200)
-                                            .grayscale(1)
-                                            
-                                        Rectangle().fill(LinearGradient(colors: [.clear, .black.opacity(0.6)], startPoint: .top, endPoint: .bottom))
-                                        VStack(alignment: .leading) {
-                                            Text(activity.name).font(.title).bold()
-                                        }
-                                            .foregroundStyle(Color.white)
-                                            .padding()
-                                    }
-
-                                }
-                                    .cornerRadius(15)
-                                    .padding(.horizontal)
-                            }
-                        }
-                        
-            }
         }
     }
 }
@@ -98,3 +60,4 @@ struct ActivitiesView: View {
 #Preview {
     ActivitiesView()
 }
+
